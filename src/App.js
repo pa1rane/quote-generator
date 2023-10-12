aefd4f7034fa4e4f19c9e5481a068ee8bc54e8af
Pavan Rane <ranepavanrp@gmail.com>
2023-10-12 18:22:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect } from 'react'
 
-function App() {
+const App = () => {
+
+  const [category, setCategory] = useState("")
+  const [quote, setQuote] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [inputVal, setInputVal] = useState("")
+
+  useEffect(() => {
+    const url = `https://api.api-ninjas.com/v1/quotes?category=${category}`
+    fetch(url,{
+      headers: {
+        'X-Api-Key' : "cllL6trlSi7gAW1tXu8mYB3ISBACbdMabF723S3i",
+      },
+    }).then((res) => res.json())
+      .then((data)=> {
+       setQuote(data)
+       setLoading(false)
+      })
+      .catch((error) => console.log(error))
+  }, [category])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      )
+      :
+      (
+      <div>
+     {quote.map((item, index) => (
+        <div key={index}>
+          <h3>{item.quote}</h3>
+          <p>{item.author}</p>
+           </div>
+      ))}
+      <p><strong>tags: </strong><input type="text" value={inputVal} onChange={(e) => setInputVal(e.target.value)}/></p>
+      <button onClick={() =>setCategory(inputVal)}>Generate</button>
+      </div>
+      )
+     }</div>
+  )
 }
 
 export default App;
